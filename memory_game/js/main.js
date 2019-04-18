5b83e3d2a0ea3554cf6a25711a33a160f1d40174
elizadavis <elizadavis@github.com>
2019-04-18 19:47:31 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,15 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "king";
-var cardFour = "queen";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[2];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2 && cardOne === cardTwo) {
+    alert("You found a match!");
+} else {
+        alert("Sorry, try again");
+    }
