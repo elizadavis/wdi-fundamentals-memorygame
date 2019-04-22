22eb68998a2c85b49318c3661bc2d04e49780d95
elizadavis <elizadavis@github.com>
2019-04-22 00:04:56 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -31,16 +31,27 @@ function checkForMatch() {
         }
     }
 
-function flipCard(cardID) {
-    console.log("User flipped " + cards[cardID].rank);
-    cardsInPlay.push(cards[cardID].rank);
+function flipCard() {
+    var cardId = this.getAttribute('data-id');
+    console.log("User flipped " + cards[cardId].rank);
+    cardsInPlay.push(cards[cardId].rank);
+    this.setAttribute('src', cards[cardId].cardImage);
     if (cardsInPlay.length === 2) {
     checkForMatch();
     }
-    console.log(cards[cardID].cardImage);
-    console.log(cards[cardID].suit);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
 }
 
+function createBoard() {
+    for (i = 0; i < cards.length; i++) {
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+    }
+}
 
-flipCard(0);
-flipCard(2);
+createBoard();
+
